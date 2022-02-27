6700b384cf03556ccac8de83df4f0566cc2ff73e
SheikhAman <amansk664@gmail.com>
2022-02-27 19:50:54 +0600

    ListView.builder

---
+++ listview_builder/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(body: ListViewBuilder()),
+    );
+  }
+}
+
+class ListViewBuilder extends StatelessWidget {
+  const ListViewBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final numItems = 20;
+    final _biggerFont = const TextStyle(fontSize: 18.0);
+
+    Widget _buildRow(int idx) {
+      return ListTile(
+        leading: CircleAvatar(
+          child: Text('$idx'),
+        ),
+        title: Text(
+          'Deger $idx',
+          style: _biggerFont,
+        ),
+        trailing: Icon(Icons.dashboard),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: numItems * 2,
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext context, int i) {
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2 + 1;
+        return _buildRow(index);
+      },
+    );
+  }
+}
